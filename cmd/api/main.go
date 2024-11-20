b377560f8a8eee941fafb1c419bf5d99731c5e34
kandlagifari <kandlagifari@gmail.com>
2024-11-20 23:04:36 +0700

    Add chi route for health check

---
+++ cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "log"
+
+func main() {
+	cfg := config{
+		addr: ":6969",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
